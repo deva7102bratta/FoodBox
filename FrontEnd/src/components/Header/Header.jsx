a7f538e5a9303d4ed554f8c4ec1fe0e05c102039
deva7102bratta <devabrattayumnam@gmail.com>
2026-06-17 11:12:32 +0530

    updated

--- FrontEnd/src/components/Header/Header.jsx
+++ FrontEnd/src/components/Header/Header.jsx
@@ -1,17 +1,26 @@
-import React from "react"
-import "./Header.css"
+import React from "react";
+import "./Header.css";
+import headerImg from "../../assets/header_img.png";
 
-const Header = ()=>{
+const Header = () => {
   return (
-    <div className="header">
+    <div
+      className="header"
+      style={{ backgroundImage: `url(${headerImg})` }}
+    >
       <div className="header-contents">
         <h1>Order Your Favorite Food Here</h1>
         <p>
-          Explore our diverse menu featuring a delicious selection of dishes, carefully crafted with the finest ingredients and exceptional culinary expertise. Whether you're craving comfort food or something new and exciting, we're here to satisfy your appetite and elevate your dining experience—one delightful meal at a time.
+          Explore our diverse menu featuring a delicious selection of dishes,
+          carefully crafted with the finest ingredients and exceptional culinary
+          expertise. Whether you're craving comfort food or something new and
+          exciting, we're here to satisfy your appetite and elevate your dining
+          experience—one delightful meal at a time.
         </p>
         <button>View Menu</button>
       </div>
     </div>
-  )
-}
-export default Header
+  );
+};
+
+export default Header;
